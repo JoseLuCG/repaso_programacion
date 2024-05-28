de0e5c39e2744ff5471f400b3e2349451d81697c
Jose Luis <joseluisconde@protonmail.com>
2024-05-28 10:45:37 +0200

    Create the methods and atributes for blade Class

---
+++ src/main/java/blades/AttackBlade.java
@@ -0,0 +1,2 @@
+package blades;public class AttackBlade {
+}
--- src/main/java/blades/Blade.java
+++ src/main/java/blades/Blade.java
@@ -1,5 +1,48 @@
 package blades;
 
-public class Blade {
+import blades.interfaces.Warrable;
 
+public abstract class Blade implements Warrable {
+    // Attributes
+    protected String id;
+    protected String name;
+    protected String color;
+    protected String type;
+    protected int SpeedSpin;
+    protected int totalPower;
+
+    // Constructors
+    public Blade() { }
+
+    public Blade(String id, String name, String color, String type) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.type = type;
+        this.SpeedSpin = 0;
+        this.totalPower = 0;
+    }
+
+    // Setters
+
+    public void setId(String id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setColor(String color) { this.color = color; }
+    public void setType(String type) { this.type = type; }
+    public void setSpeedSpin(int speedSpin) { SpeedSpin = speedSpin; }
+    public void setTotalPower(int totalPower) { this.totalPower = totalPower; }
+
+    // Getters
+
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getColor() { return color; }
+    public String getType() { return type; }
+
+    public int getSpeedSpin() { return SpeedSpin; }
+    public int getTotalPower() { return totalPower; }
+
+    // Methods
+    abstract void showBladeData();
 }

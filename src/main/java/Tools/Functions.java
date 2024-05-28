9799c5eb17db19b2e40403558fc5879c4459328c
Jose Luis <joseluisconde@protonmail.com>
2024-05-28 11:20:48 +0200

    Add a printStream function

--- src/main/java/Tools/Functions.java
+++ src/main/java/Tools/Functions.java
@@ -1,2 +1,8 @@
-package Tools;public class Functions {
+package Tools;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+public class Functions {
+    public static PrintStream outPut = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 }

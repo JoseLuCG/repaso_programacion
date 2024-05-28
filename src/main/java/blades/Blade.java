3c152520c2845ddea87436d71ed0b7c7c54d2bdb
Jose Luis <joseluisconde@protonmail.com>
2024-05-28 10:30:23 +0200

    Creates a blade Class and implements the methods of the Warrable interface

---
+++ src/main/java/blades/Blade.java
@@ -0,0 +1,5 @@
+package blades;
+
+public class Blade {
+
+}
---
+++ src/main/java/blades/interfaces/Warrable.java
@@ -0,0 +1,8 @@
+package blades.interfaces;
+
+import blades.interfaces.Blade;
+
+public interface Warrable {
+    public void throwBlade (int speed);
+    public void attackblade (Blade obj);
+}
